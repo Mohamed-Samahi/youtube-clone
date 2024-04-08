c53a7688df93bbf1a4ba385fe3d39e20c8306cb8
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-04-08 09:42:48 +0200

    adding video play on hover feature

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
+import { Card, CardContent, CardMedia, Typography, IconButton } from "@mui/material";
+import { CheckCircle, PlayCircleOutline, VolumeOff, VolumeUp } from "@mui/icons-material";
+import ReactPlayer from "react-player";
 
 import {
   demoVideoUrl,
@@ -16,6 +17,9 @@ const VideoCard = ({
     snippet,
   },
 }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isMuted, setIsMuted] = useState(true);
+
   return (
     <Card
       sx={{
@@ -24,13 +28,61 @@ const VideoCard = ({
         borderRadius: 0,
       }}
     >
-      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <CardMedia
-          image={snippet?.thumbnails?.high?.url}
-          alt={snippet?.title}
-          sx={{ width: { xs: "100%", sm: "358px", md: "310px" }, height: 180 }}
-        />
-      </Link>
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{ position: "relative" }}
+      >
+        <Link
+          to={videoId ? `/video/${videoId}` : demoVideoUrl}
+        >
+          {!isHovered ?
+            <CardMedia
+              image={snippet?.thumbnails?.high?.url}
+              alt={snippet?.title}
+              sx={{ width: { xs: "100%", sm: "358px", md: "310px" }, height: 180 }}
+            />
+            :
+            <div style={{ height: "180px", overflow: "hidden", position: "relative" }}>
+              <ReactPlayer
+                url={`https://www.youtube.com/watch?v=${videoId}`}
+                className="react-player"
+                playing={true}
+                muted={isMuted}
+                width="100%"
+                height="100%"
+                controls={false}
+                config={{
+                  youtube: {
+                    playerVars: {
+                      modestbranding: 1,
+                      showinfo: 0,
+                      controls: 0,
+                      autoplay: 1
+                    },
+                  },
+                }}
+                wrapper="div"
+                style={{ objectFit: "cover", position: "absolute", top: "0", left: "0", right: "0", bottom: "0" }}
+              />
+            </div>
+          }
+        </Link>
+        {isHovered && (
+          <IconButton
+            onClick={() => setIsMuted(prev => !prev)}
+            sx={{
+              position: "absolute",
+              top: "5px",
+              right: "5px",
+              color: "white",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+            }}
+          >
+            {isMuted ? <VolumeOff /> : <VolumeUp />}
+          </IconButton>
+        )}
+      </div>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "90px" }}>
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
           <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
